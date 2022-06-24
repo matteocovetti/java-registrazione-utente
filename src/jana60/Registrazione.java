02481ea67f31e1605ed275267cd009553b9e1110
matteocovetti <covetti.matteo@gmail.com>
2022-06-24 17:16:39 +0200

    first commit

---
+++ src/jana60/Registrazione.java
@@ -0,0 +1,15 @@
+package jana60;
+
+public class Registrazione {
+	/*
+	 * Creare una classe Registrazione con metodo main in cui, tramite Scanner,
+	 * vengono chieste le informazioni all’utente e si prova a creare un nuovo
+	 * oggetto Utente. Gestire tramite try e catch eventuali errori in fase di
+	 * creazione in modo che il programma non si interrompa bruscamente ma chieda
+	 * all’utente di inserire dei dati corretti.
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/jana60/Utente.java
@@ -0,0 +1,13 @@
+package jana60;
+
+public class Utente {
+	/*
+	 * Creare una classe Utente con attributi nome, cognome, email, password ed età.
+	 * Aggiungere un costruttore con parametri e metodi getter e setter.
+	 * Implementare dei metodi di validazione in modo che: - email contenga un
+	 * carattere @ e un . - password sia lunga tra 8 e 12 caratteri - età sia
+	 * maggiore di 18 anni In caso i parametri non siano validi, impedire che
+	 * vengano valorizzati gli attributi nei metodi e nel costruttore, sollevando
+	 * opportune eccezioni.
+	 */
+}
