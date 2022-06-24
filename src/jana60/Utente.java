dcf9d486227b28eaa8ed195624bc4cd16fcb04fd
matteocovetti <covetti.matteo@gmail.com>
2022-06-24 17:31:12 +0200

    add utente

--- src/jana60/Utente.java
+++ src/jana60/Utente.java
@@ -10,4 +10,75 @@ public class Utente {
 	 * vengano valorizzati gli attributi nei metodi e nel costruttore, sollevando
 	 * opportune eccezioni.
 	 */
+
+	// attributi
+	private String nome, cognome, email, password;
+	private int eta;
+
+	// costruttore
+	public Utente(String nome, String cognome, String email, String password, int eta) throws Exception {
+		super();
+
+		if (password.length() < 18 && password.length() > 12) {
+			throw new Exception("La password deve essere minimo di 8 e massimo di 12 caratteri");
+		}
+		if (eta < 18) {
+			throw new Exception("L'utente deve essere maggiorenne per potersi registrare");
+		}
+		if (!email.contains("@")) {
+			throw new Exception("L'email deve contenere il carattere @");
+		}
+		if (!email.contains(".")) {
+			throw new Exception("L'emeil deve contenere un .");
+		}
+
+		this.nome = nome;
+		this.cognome = cognome;
+		this.email = email;
+		this.password = password;
+		this.eta = eta;
+	}
+
+	// getter e setter
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getEta() {
+		return eta;
+	}
+
+	public void setEta(int eta) {
+		this.eta = eta;
+	}
+
 }
